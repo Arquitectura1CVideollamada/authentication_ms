ce63f77c9cdf4b62a86b08054123d5b09403b83a
aeromeror <aeromeror@unal.edu.co>
2022-01-13 07:32:47 -0500

    db

--- dist/database.js
+++ dist/database.js
@@ -7,9 +7,8 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ path: '../env' });
 //require('dotenv').config({path:'../env'});
-mongoose_1.default.connect(process.env.DB_URL, {
+//mongoose.connect(process.env.DB_URL!,{
 //mongoose.connect('mongodb+srv://user:pass@cluster0.dclce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
 //mongoose.connect('mongodb://localhost/auth_db',{
-//mongoose.connect('mongodb://mongo:27018/auth_db',{
-}).then(db => console.log('database conected')).catch(err => console.log(err));
+mongoose_1.default.connect('mongodb://mongo:27018/auth_db', {}).then(db => console.log('database conected')).catch(err => console.log(err));
 //# sourceMappingURL=database.js.map
--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ dotenv.config({ path:'../env' });
 
 //mongoose.connect(process.env.DB_URL!,{
 //mongoose.connect('mongodb+srv://user:pass@cluster0.dclce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-mongoose.connect('mongodb://localhost/auth_db',{
-//mongoose.connect('mongodb://mongo:27018/auth_db',{
+//mongoose.connect('mongodb://localhost/auth_db',{
+mongoose.connect('mongodb://mongo:27018/auth_db',{
 
 }).then(db => console.log('database conected')).catch(err =>console.log(err));
